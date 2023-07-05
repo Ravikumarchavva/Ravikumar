6f4042547ee07cb11130041296665f0e96719754
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2023-07-05 09:43:21 +0530

    akhil

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:91852fb9a02d67f29abebfe64fa55d5fdaf4a8c845c5e3e4408e5b21c91059ef
-size 233
+FROM python:3.10.12-slim
+WORKDIR /app
+COPY . /app/
+RUN pip install -r requirements.txt
+EXPOSE 5000
+CMD python ./main.py
--- ipl_cloud.py
+++ ipl_cloud.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 from google.cloud import storage
 # Set the path to your service account key file
-key_file = "/workspace/Ravikumar/healthy-genre-391115-f36176b04acf.json"
+key_file = "healthy-genre-391115-f36176b04acf.json"
 
 # Set the environment variable for the key file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_file
@@ -58,9 +58,9 @@ def ipl_predict(inning,bat,bowl,city_input,toss_winner,toss_taken,current_runs,c
         toss_decision=0
 
     current_run_rate=int(current_runs)/int(round(float(current_ball)))
-    print(inning,ba_t,bo_t,city,toss,toss_decision,current_ball,current_runs,current_wickets,current_run_rate)
+    #print(inning,ba_t,bo_t,city,toss,toss_decision,current_ball,current_runs,current_wickets,current_run_rate)
     values=[int(inning)]+ba_t+bo_t+city+toss+[int(toss_decision),int(round(float(current_ball))),int(current_runs),int(current_wickets),float(current_run_rate)]
-    print(values)
+    #print(values)
     keys=['inning', 'batting_team_Chennai Super Kings', 'batting_team_Delhi Capitals', 'batting_team_Gujarat Lions', 'batting_team_Kings XI Punjab', 'batting_team_Kolkata Knight Riders', 'batting_team_Mumbai Indians', 'batting_team_Pune Warriors', 'batting_team_Rajasthan Royals', 'batting_team_Royal Challengers Bangalore', 'batting_team_Sunrisers Hyderabad', 'bowling_team_Chennai Super Kings', 'bowling_team_Delhi Capitals', 'bowling_team_Gujarat Lions', 'bowling_team_Kings XI Punjab', 'bowling_team_Kolkata Knight Riders', 'bowling_team_Mumbai Indians', 'bowling_team_Pune Warriors', 'bowling_team_Rajasthan Royals', 'bowling_team_Royal Challengers Bangalore', 'bowling_team_Sunrisers Hyderabad', 'city_Ahmedabad', 'city_Bangalore', 'city_Chandigarh', 'city_Chennai', 'city_Delhi', 'city_Dharamsala', 'city_Hyderabad', 'city_Jaipur', 'city_Kolkata', 'city_Mohali', 'city_Mumbai', 'city_Pune', 'toss_winner_Chennai Super Kings', 'toss_winner_Delhi Capitals', 'toss_winner_Gujarat Lions', 'toss_winner_Kings XI Punjab', 'toss_winner_Kolkata Knight Riders', 'toss_winner_Mumbai Indians', 'toss_winner_Pune Warriors', 'toss_winner_Rajasthan Royals', 'toss_winner_Royal Challengers Bangalore', 'toss_winner_Sunrisers Hyderabad', 'toss_decision', 'balls', 'current_runs', 'wickets', 'current_run_rate']
     input_list = {k: {1: v} for k, v in zip(keys,values)}
     s=m.predict(input_list)
